900a69fc5902a093ec83318121e37b8e97082b57
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-02-22 13:55:34 +0530

    added Entity

---
+++ src/main/java/com/springdatajpa/springdatajpa/entity/Product.java
@@ -0,0 +1,59 @@
+package com.springdatajpa.springdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "sku_unique",
+                        columnNames = "stock_keeping_unit"
+                )
+        }
+)
+
+public class Product {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_generator"
+    )
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
+    private Long Id;
+
+    @Column(name = "stock_keeping_unit", nullable = false)
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
+    private String imageURl;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
