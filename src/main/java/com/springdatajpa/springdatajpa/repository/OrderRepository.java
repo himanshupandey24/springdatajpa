9ab2591dd884cc3707ac8be9d554ce7f5108ce6c
Himanshu Pandey <himanshupandey2411@gmail.com>
2023-02-28 13:59:39 +0530

    added @Transactional

--- src/main/java/com/springdatajpa/springdatajpa/repository/OrderRepository.java
+++ src/main/java/com/springdatajpa/springdatajpa/repository/OrderRepository.java
@@ -2,7 +2,17 @@ package com.springdatajpa.springdatajpa.repository;
 
 import com.springdatajpa.springdatajpa.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findByOrderTrackingNumber(String orderTrackingNumber);
+
+   /* @Override
+    @Transactional(timeout = 10)
+    void deleteById(Long aLong);
+
+    @Override
+    @Transactional(propagation = Propagation.NEVER)
+    <S extends Order> S save(S entity);*/
 }
